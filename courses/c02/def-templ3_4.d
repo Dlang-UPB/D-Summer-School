6a4eefcd1e4a54191df7f64e4180d14970602068
DoctorNoobingstoneIPresume <DoctorNoobingstoneIPresume@gmail.com>
2021-07-13 21:22:17 +0300

    courses/c02/def-templ3_4.d: Attempt to fix-and-test fib (Fibonacci).

--- courses/c02/def-templ3_4.d
+++ courses/c02/def-templ3_4.d
@@ -1,24 +1,26 @@
 module def_templ;
 
-import std.stdio;
-
-T getResponse(T = int)(string question)
+ulong fib(uint N)()
 {
-    writef("%s (%s): ", question, T.stringof);
-    T response;
-    readf(" %s", &response);
-
-    return response;
+    static if (N <= 1)
+        return N;
+    else
+        return fib!(N - 2)() + fib!(N - 1)();
 }
 
-int fib(int N)()
+void fibDisplay(uint N)()
 {
-    if (N == 0)
-        return 0;
-    return N + fib!(N - 1)();
+    static if (N > 0)
+        fibDisplay!(N - 1)();
+
+    import std.stdio;
+    writef("fib ! %2u () == %19u\n", N, fib!N());
 }
 
 void main(string[] args)
 {
+    fibDisplay!45();
+
     auto x = fib!(10)();
+    assert(x == 55);
 }
