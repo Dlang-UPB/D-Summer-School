781158ef1b0ba46a4ec9b86f7c8f407755a55135
RazvanN7 <razvan.nitu1305@gmail.com>
2022-10-25 12:45:22 +0300

    Fix lab 1 issues

--- Introduction to D/lecture/arrays/array_setting.d
+++ Introduction to D/lecture/arrays/array_setting.d
@@ -5,7 +5,7 @@ void main()
     int[] b = [1, 2, 3, 4, 5];
     a = 3;                     // set all elements of a to 3
     a[] = 2;                   // same as `a = 3`; using an empty slice is the same as slicing the full array
-    b = a[0 .. $];             // $ evaluates to the length of the array (in this case 10)
+    b = a[0 .. $];             // $ evaluates to the length of the array (in this case 3)
     b = a[];                   // semantically equivalent to the one above
     b = a[0 .. a.length];      // semantically equivalent to the one above
     b[] = a[];                 // semantically equivalent to the one above
